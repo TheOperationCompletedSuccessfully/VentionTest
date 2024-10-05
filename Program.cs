487c4148cf9a8733190fa544317cd1c2d5e8b4f8
Andriy Zyevakov <andriy.zyevakov@protonmail.com>
2024-10-05 15:58:11 +0200

    Test

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Text;
+
+var test = Console.ReadLine();
+
+var result = Transform(test);
+Console.WriteLine(result);
+
+    static string Transform(string value)
+    {
+        var data = value.ToCharArray();
+        var builder = new StringBuilder();
+        for (int i = 0; i < data.Length; i++)
+        {
+            if (data[i] >= 97 && data[i] <= 122)
+            {
+                if (data[i] == 122)
+                {
+                    builder.Append(97);
+                }
+                else
+                {
+                    builder.Append((char)(data[i] + 1));
+                }
+            }
+            else
+            {
+                builder.Append(data[i]);
+            }
+        }
+        return builder.ToString();
+    }
+
